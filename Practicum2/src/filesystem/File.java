2caa6cf1d7803b6a41e038956b86a3ff3bed22b9
csvBoglarka <csvboglarka@gmail.com>
2026-04-02 23:33:33 +0200

    size verplaatst (van DiskItem naar File)

--- Practicum2/src/filesystem/File.java
+++ Practicum2/src/filesystem/File.java
@@ -111,6 +111,121 @@ public class File extends DiskItem {
 
 
     /**********************************************************
+     * size - nominal programming
+     **********************************************************/
+
+    /**
+     * Variable registering the size of this disk item (in bytes).
+     */
+    private int size = 0;
+
+    /**
+     * Variable registering the maximum size of any disk item (in bytes).
+     */
+    private static final int maximumSize = Integer.MAX_VALUE;
+
+
+    /**
+     * Return the size of this disk item (in bytes).
+     */
+    @Raw @Basic
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Set the size of this disk item to the given size.
+     *
+     * @param  size
+     *         The new size for this disk item.
+     * @pre    The given size must be legal.
+     *         | isValidSize(size)
+     * @post   The given size is registered as the size of this disk item.
+     *         | new.getSize() == size
+     */
+    @Raw @Model
+    protected void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     * Return the maximum disk item size.
+     */
+    @Basic @Immutable
+    public static int getMaximumSize() {
+        return maximumSize;
+    }
+
+    /**
+     * Check whether the given size is a valid size for a disk item.
+     *
+     * @param  size
+     *         The size to check.
+     * @return True if and only if the given size is positive and does not
+     *         exceed the maximum size.
+     *         | result == ((size >= 0) && (size <= getMaximumSize()))
+     */
+    public static boolean isValidSize(int size) {
+        return ((size >= 0) && (size <= getMaximumSize()));
+    }
+
+    /**
+     * Increases the size of this disk item with the given delta.
+     *
+     * @param   delta
+     *          The amount of bytes by which the size of this disk item
+     *          must be increased.
+     * @pre     The given delta must be strictly positive.
+     *          | delta > 0
+     * @effect  The size of this disk item is increased with the given delta.
+     *          | changeSize(delta)
+     */
+    public void enlarge(int delta) throws DiskItemNotWritableException {
+        changeSize(delta);
+    }
+
+    /**
+     * Decreases the size of this disk item with the given delta.
+     *
+     * @param   delta
+     *          The amount of bytes by which the size of this disk item
+     *          must be decreased.
+     * @pre     The given delta must be strictly positive.
+     *          | delta > 0
+     * @effect  The size of this disk item is decreased with the given delta.
+     *          | changeSize(-delta)
+     */
+    public void shorten(int delta) throws DiskItemNotWritableException {
+        changeSize(-delta);
+    }
+
+    /**
+     * Change the size of this disk item with the given delta.
+     *
+     * @param  delta
+     *         The amount of bytes by which the size of this disk item
+     *         must be increased or decreased.
+     * @pre    The given delta must not be 0
+     *         | delta != 0
+     * @effect The size of this disk item is adapted with the given delta.
+     *         | setSize(getSize()+delta)
+     * @effect The modification time is updated.
+     *         | setModificationTime()
+     * @throws DiskItemNotWritableException(this)
+     *         This disk item is not writable.
+     *         | ! isWritable()
+     */
+    @Model
+    private void changeSize(int delta) throws DiskItemNotWritableException{
+        if (isWritable()) {
+            setSize(getSize()+delta);
+            setModificationTime();
+        }else{
+            throw new DiskItemNotWritableException(this);
+        }
+    }
+
+    /**********************************************************
      * destructors
      **********************************************************/
 
